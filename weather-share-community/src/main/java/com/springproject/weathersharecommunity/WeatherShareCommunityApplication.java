ae630fcf17b288acd0a52b1f7ca6043686cedb76
subn0206 <2149ksb@naver.com>
2021-12-26 02:10:07 +0900

    init commit

---
+++ weather-share-community/src/main/java/com/springproject/weathersharecommunity/WeatherShareCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.weathersharecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherShareCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherShareCommunityApplication.class, args);
+	}
+
+}
---
+++ weather-share-community/src/test/java/com/springproject/weathersharecommunity/WeatherShareCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.weathersharecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherShareCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
